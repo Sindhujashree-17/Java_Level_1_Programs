a96be6efc76520729771f1dac718d435722b68e9
Sindhujashree R <151825070+Sindhujashree-17@users.noreply.github.com>
2026-09-24 19:49:12 +0530

    Update Problem21_Subtract5IfOdd.java

--- Problem21_Subtract5IfOdd.java
+++ Problem21_Subtract5IfOdd.java
@@ -8,7 +8,6 @@ public class Problem21_Subtract5IfOdd {
 
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
-
         int result = num - (num % 2) * 5;
         System.out.println("Result: " + result);
     }
